d330649b132a9f454e9b066972c8b28ed9e59d53
Mysa33 <tpdrn.france@gmail.com>
2015-12-20 18:18:50 +0100

    Portfolio filtrable
    Portfolio filtrable, jquery, bootstrap3, html5, css3

---
+++ js/filtrable.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+			
+function bouton1(){
+			$("#PARTI1,#PARTI2").show(500,'swing');	
+			
+			}
+			
+function bouton2(){
+			$( "#PARTI1, #PARTI3" ).show(500, 'swing');	
+			$("#PARTI2, #PART3").hide(500,'swing');
+			}
+			
+function bouton3(){
+			$("#PARTI2, #PART3").show(1500,'swing');	
+			$("#PARTI1, #PART3").hide(500,'swing');
+			}
